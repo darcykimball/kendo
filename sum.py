aab5bf757000baaa49c8abd122bb04c8a2fab062
Darcy Kimball <darcy@rh-ip-1876-res.ucsd.edu>
2016-06-02 16:00:35 -0700

    Added delay/work time sleeping to SumProcess

--- sum.py
+++ sum.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python2
 
+import random
+import time
+
 class SumProcess():
     """A thread that computes part of a sum."""
 
-    def __init__(self, arbitrator, lock_num, nums):
+    def __init__(self, arbitrator, lock_num, nums, delay_time=0, work_time=0):
         """Construct a SumProcess.
 
         Args:
         arbitrator - the kendo (or other) arbitrator
         lock_num - lock protecting shared sum
         nums - iterable of numbers to addup
+        delay_time - time to delay before starting work
+        work_time - simulated time it takes to complete the work
         """
 
         self.arbitrator = arbitrator
         self.lock_num = lock_num
         self.nums = nums
+        self.delay_time = delay_time
+        self.work_time = work_time
 
         # Initialize the shared memory total
         # XXX: this is redundant since multiple threads will do this, but it's
@@ -29,10 +36,16 @@ class SumProcess():
         done.
         """
 
+        # Delay a bit.
+        time.sleep(self.delay_time)
+
         print "SumProcess, PID = ", self.pid, " starting..."
 
         # Do the calculation
         local_total = sum(self.nums)
+
+        # Simulate time-consuming work
+        time.sleep(self.work_time)
 
         print "SumProcess, PID = ", self.pid, "local_total = ", local_total
 
@@ -57,10 +70,10 @@ if __name__ == "__main__":
 
     kendo_arbitrator = kendo.Kendo(max_processes=4, num_locks=1, debug=True)
 
-    process1 = SumProcess(kendo_arbitrator, 0, xrange(10000))
-    process2 = SumProcess(kendo_arbitrator, 0, xrange(10000, 20000))
-    process3 = SumProcess(kendo_arbitrator, 0, xrange(20000, 30000))
-    process4 = SumProcess(kendo_arbitrator, 0, xrange(30000, 40000))
+    process1 = SumProcess(kendo_arbitrator, 0, xrange(10000), work_time=random.random())
+    process2 = SumProcess(kendo_arbitrator, 0, xrange(10000, 20000), work_time=random.random())
+    process3 = SumProcess(kendo_arbitrator, 0, xrange(20000, 30000), work_time=random.random())
+    process4 = SumProcess(kendo_arbitrator, 0, xrange(30000, 40000), work_time=random.random())
     
     kendo_arbitrator.run()
 
